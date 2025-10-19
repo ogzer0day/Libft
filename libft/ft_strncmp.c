0ce08db6617da5e890ed815719eeb4c61d19b9fa
Zero <ZERO_BROTHER12345@outlook.com>
2025-10-19 15:59:18 +0100

    Update ft_strncmp.c

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,16 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzougari <mzougari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 08:27:48 by mzougari          #+#    #+#             */
+/*   Updated: 2025/10/17 08:41:13 by mzougari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int	i;
 
+	i = 0;
 	while (s1[i] && s2[i] && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	if (i < n)
-		return (s1[i] - s2[i]);
-
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	return (0);
 }
