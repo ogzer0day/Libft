e458818e94968a8b20ddde31f5cc56322a34e09d
Zero <ZERO_BROTHER12345@outlook.com>
2025-10-19 15:53:53 +0100

    Update ft_memmove.c

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,13 +1,40 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzougari <mzougari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 08:19:40 by mzougari          #+#    #+#             */
+/*   Updated: 2025/10/18 13:45:05 by mzougari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void	*memmove(void *dest, const void *src, size_t n)
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t i = 0;
-	unsigned char *des = (unsigned char *)dest;
-	const unsigned char *sr = (const unsigned char *)src;
+	char	*src1;
+	char	*dest1;
+	size_t	i;
 
-	while (i < n)
-		*des++ = sr[i++];
-
-	return (dest);
+	if (!dst || !src)
+		return (NULL);
+	src1 = (char *)src;
+	dest1 = (char *)dst;
+	if (dest1 > src1)
+	{
+		while (len > 0)
+		{
+			len--;
+			dest1[len] = src1[len];
+		}
+	}
+	else
+	{
+		i = -1;
+		while (++i < len)
+			dest1[i] = src1[i];
+	}
+	return (dst);
 }
