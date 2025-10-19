3f490c26244ca3d44205b847b2ceeb3741487505
Zero <ZERO_BROTHER12345@outlook.com>
2025-10-19 15:52:10 +0100

    Update ft_lstmap_bonus.c

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -1,69 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzougari <mzougari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 08:18:30 by mzougari          #+#    #+#             */
+/*   Updated: 2025/10/19 15:27:10 by mzougari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
-#include <stdlib.h>
-
-void	ft_lstadd_back(t_list **lst, t_list *new)
-{
-	t_list	*temp;
-
-	if (!new || !lst)
-		return ;
-	if (!*lst)
-	{
-		*lst = new;
-		return ;
-	}
-	temp = *lst;
-	while (temp->next)
-		temp = temp->next;
-	temp->next = new;
-}
-
-void	ft_lstclear(t_list **lst, void (*del)(void *))
-{
-	t_list	*temp;
-	t_list	*next_node;
-
-	if (!lst || !*lst)
-		return ;
-	temp = *lst;
-	while (temp)
-	{
-		next_node = temp->next;
-		del(temp->content);
-		free(temp);
-		temp = next_node;
-	}
-	*lst = NULL;
-}
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*head;
-	t_list	*temp;
+	t_list	*new_list;
 	t_list	*new_node;
+	void	*content;
 
 	if (!lst || !f)
 		return (NULL);
-	head = NULL;
-	temp = lst;
-	while (temp)
+	new_list = NULL;
+	while (lst)
 	{
-		new_node = (t_list *)malloc(sizeof(t_list));
+		content = f(lst->content);
+		if (!content)
+			return (ft_lstclear(&new_list, del), NULL);
+		new_node = ft_lstnew(content);
 		if (!new_node)
 		{
-			ft_lstclear(&head, del);
+			del(content);
+			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
-		new_node->content = f(temp->content);
-		if (!new_node->content)
-		{
-			free(new_node);
-			ft_lstclear(&head, del);
-			return (NULL);
-		}
-		new_node->next = NULL;
-		ft_lstadd_back(&head, new_node);
-		temp = temp->next;
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
 	}
-	return (head);
+	return (new_list);
 }
