4d88118ef3943068d4c9d882c12c5e84432e8279
Zero <ZERO_BROTHER12345@outlook.com>
2025-10-19 15:53:17 +0100

    Update ft_memcmp.c

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -1,13 +1,25 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzougari <mzougari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 08:19:23 by mzougari          #+#    #+#             */
+/*   Updated: 2025/10/17 09:10:14 by mzougari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	memcmp(const void *s1, const void *s2, size_t n)
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const unsigned char	*p1;
-	const unsigned char	*p2;
-	size_t				i;
+	unsigned char	*p1;
+	unsigned char	*p2;
+	size_t			i;
 
-	p1 = (const unsigned char *)s1;
-	p2 = (const unsigned char *)s2;
+	p1 = (unsigned char *)s1;
+	p2 = (unsigned char *)s2;
 	i = 0;
 	while (i < n)
 	{
@@ -15,5 +27,7 @@ int	memcmp(const void *s1, const void *s2, size_t n)
 			return (p1[i] - p2[i]);
 		i++;
 	}
+	if (i < n)
+		return (p1[i] - p2[i]);
 	return (0);
 }
