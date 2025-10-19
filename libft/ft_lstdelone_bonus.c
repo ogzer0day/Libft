95c2a456df9220a3ecdd9b20c03a2d1907bc3d28
Zero <ZERO_BROTHER12345@outlook.com>
2025-10-19 15:51:16 +0100

    Update ft_lstdelone_bonus.c

--- libft/ft_lstdelone_bonus.c
+++ libft/ft_lstdelone_bonus.c
@@ -1,6 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzougari <mzougari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 08:18:00 by mzougari          #+#    #+#             */
+/*   Updated: 2025/10/19 10:17:12 by mzougari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
@@ -8,46 +18,4 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 		return ;
 	del(lst->content);
 	free(lst);
-}
-
-void	del(void *node)
-{
-	free(node);
-}
-
-void	print_nodes(t_list *nodes)
-{
-	while (nodes)
-	{
-		printf("%d\n", *(int *)nodes->content);
-		nodes = nodes->next;
-	}
-}
-
-int	main(void)
-{
-	t_list *head = (t_list *)malloc(sizeof(t_list));
-	t_list *secand = (t_list *)malloc(sizeof(t_list));
-	t_list *third = (t_list *)malloc(sizeof(t_list));
-
-	int *a = malloc(sizeof(int));
-	int *b = malloc(sizeof(int));
-	int *c = malloc(sizeof(int));
-
-	*a = 5;
-	*b = 10;
-	*c = 15;
-
-	head->content = a;
-	head->next = secand;
-
-	secand->content = b;
-	secand->next = third;
-
-	third->content = c;
-	third->next = NULL;
-
-	ft_lstdelone(head, del);
-
-	print_nodes(secand);
 }
