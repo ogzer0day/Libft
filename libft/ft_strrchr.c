f5fd386d84981508e87216cf180533d963fe8d73
Zero <ZERO_BROTHER12345@outlook.com>
2025-10-19 15:59:58 +0100

    Update ft_strrchr.c

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -1,27 +1,31 @@
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzougari <mzougari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 08:29:59 by mzougari          #+#    #+#             */
+/*   Updated: 2025/10/17 16:22:01 by mzougari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	ft_strlen(const char *str)
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
 {
-	int	len;
+	size_t	i;
 
-	len = 0;
-	while (str[len])
-		len++;
-	return (len);
-}
-
-char	*strrchr(const char *str, int c)
-{
-	char	*p;
-	int		i;
-
-	p = (char *)str;
-	i = ft_strlen(p);
-	while (i >= 0)
+	i = ft_strlen(s);
+	if ((unsigned char)c == '\0')
+		return ((char *)&s[i]);
+	while (i > 0)
 	{
-		if (p[i] == c)
-			return (&p[i]);
 		i--;
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+			return ((char *)&s[i]);
 	}
+	if (((unsigned char *)s)[0] == (unsigned char)c)
+		return ((char *)&s[i]);
 	return (NULL);
 }
