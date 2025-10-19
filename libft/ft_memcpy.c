3a0db1886133583fc4632ec3c786bdc0e8a3c518
Zero <ZERO_BROTHER12345@outlook.com>
2025-10-19 15:53:34 +0100

    Update ft_memcpy.c

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -1,4 +1,16 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzougari <mzougari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 08:19:31 by mzougari          #+#    #+#             */
+/*   Updated: 2025/10/16 09:08:21 by mzougari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
 
 void	*ft_memcpy(void *destination, const void *source, size_t numBytes)
 {
