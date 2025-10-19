1d17e505b5e8ab7541b534c18d29af002327987a
Zero <ZERO_BROTHER12345@outlook.com>
2025-10-19 15:52:25 +0100

    Update ft_lstnew_bonus.c

--- libft/ft_lstnew_bonus.c
+++ libft/ft_lstnew_bonus.c
@@ -1,11 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzougari <mzougari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 08:18:39 by mzougari          #+#    #+#             */
+/*   Updated: 2025/10/17 16:25:04 by mzougari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
-#include <stdlib.h>
 
 t_list	*ft_lstnew(void *content)
 {
 	t_list	*new_node;
 
-	new_node = (t_list *)malloc(sizeof(t_list));
+	new_node = malloc(sizeof(t_list));
 	if (!new_node)
 		return (NULL);
 	new_node->content = content;
