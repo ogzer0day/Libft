8f9192066aa2e976600d2ff1c9db6b33b4e5ae85
Zero <ZERO_BROTHER12345@outlook.com>
2025-10-19 15:53:02 +0100

    Update ft_memchr.c

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -1,18 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzougari <mzougari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 08:19:05 by mzougari          #+#    #+#             */
+/*   Updated: 2025/10/17 08:49:05 by mzougari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const unsigned char	*p;
-	size_t				i;
+	unsigned char	*p;
+	size_t			i;
 
-	p = (const unsigned char *)s;
+	p = (unsigned char *)s;
 	i = 0;
 	while (i < n)
 	{
-		if (p[i] == c)
+		if (p[i] == (unsigned char)c)
 			return ((void *)&p[i]);
 		i++;
 	}
-	return (NULL);
+	return (0);
 }
